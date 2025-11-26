b1d4b97673727e9876accb10f8f8513782bce4db
Damilola91 <damilola.wiwoloku@gmail.com>
2025-11-26 16:55:11 +0100

    add: Todo Form Component

--- React/vite-project/src/components/TodoForm/TodoForm.jsx
+++ React/vite-project/src/components/TodoForm/TodoForm.jsx
@@ -3,8 +3,30 @@ import Input from "../Input/Input";
 import Button from "../Button/Button";
 import { Plus } from "lucide-react";
 
-const TodoForm = () => {
+const TodoForm = ({ onAddTodo }) => {
   const [text, setText] = useState("");
 
-  const handleSubmit = (event) => {};
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const trimmedText = text.trim();
+    if (!trimmedText) return;
+    onAddTodo(trimmedText);
+    setText("");
+  };
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <Input
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+        placeholder="Aggiungi un nuovo Task..."
+      />
+      <Button type="submit">
+        <Plus size={16} />
+        Aggiungi
+      </Button>
+    </form>
+  );
 };
+
+export default TodoForm;
