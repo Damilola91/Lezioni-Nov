39843b92da744fce2ba211c41e1e3862faf53e71
Damilola91 <damilola.wiwoloku@gmail.com>
2025-12-02 15:32:24 +0100

    feat: add GameFilters component

--- GameHub/game-hub/src/components/GameFilters/GameFilters.jsx
+++ GameHub/game-hub/src/components/GameFilters/GameFilters.jsx
@@ -1,5 +1,25 @@
-const GameFilters = () => {
-  return <></>;
+const GameFilters = ({
+  genre,
+  platform,
+  minRating,
+  wishlist,
+  setGenre,
+  setPlatform,
+  setMinRating,
+  setWishlist,
+}) => {
+  return (
+    <div className="filters">
+      <select value={genre} onChange={(e) => setGenre(e.target.value)}>
+        <option value="all">All Genres</option>
+        <option value="RPG">RPG</option>
+        <option value="FPS">FPS</option>
+        <option value="Adventure">Adventure</option>
+        <option value="Sports">Sports</option>
+        <option value="Platform">Platform</option>
+      </select>
+    </div>
+  );
 };
 
 export default GameFilters;
