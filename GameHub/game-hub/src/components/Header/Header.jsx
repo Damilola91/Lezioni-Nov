f28d9782898425c761b82acf6ad6ce16b9ada618
Damilola91 <damilola.wiwoloku@gmail.com>
2025-12-02 14:56:58 +0100

    feat: add Header Component

--- GameHub/game-hub/src/components/Header/Header.jsx
+++ GameHub/game-hub/src/components/Header/Header.jsx
@@ -1,5 +1,25 @@
-const Header = () => {
-  return <></>;
+import { Gamepad2 } from "lucide-react";
+
+const Header = ({ total, search, onSearch }) => {
+  return (
+    <header className="header">
+      <h1>
+        <Gamepad2 /> Game Hub
+      </h1>
+
+      <div className="header-right">
+        <span className="badge">Giochi totali: {total}</span>
+
+        <input
+          type="text"
+          placeholder="Cerca gioco..."
+          value={search}
+          onChange={(e) => onSearch(e.target.value)}
+          className="search"
+        />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
